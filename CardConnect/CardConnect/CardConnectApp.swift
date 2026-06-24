5ba7c98284672cc5671169e76544a16a1c2ad01b
tolgabarisalcitepe <tolgabarisalcitepe@gmail.com>
2026-06-25 01:20:02 +0300

    Scaffold Xcode SwiftUI project

---
+++ CardConnect/CardConnect/CardConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardConnectApp.swift
+//  CardConnect
+//
+//  Created by RiderPanda on 24.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct CardConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CardConnect/CardConnectTests/CardConnectTests.swift
@@ -0,0 +1,19 @@
+//
+//  CardConnectTests.swift
+//  CardConnectTests
+//
+//  Created by RiderPanda on 24.06.2026.
+//
+
+import Testing
+@testable import CardConnect
+
+struct CardConnectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
